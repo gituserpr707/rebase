1c6860b7a59e020d89de92cea83341d7b01eeb04
gituserpr707 <prashantangadi1999@gmail.com>
2023-11-02 13:57:01 +0530

    5th commmit in feature branch02 - 05

--- src/TestPoc.java
+++ src/TestPoc.java
@@ -4,27 +4,21 @@ public class TestPoc {
         System.out.println("initial commit- 1.0");
     }
 
-    public void displayName(){
-
-        {
-            System.out.println("second commit- 1.0");
-        }
+    public void displayName() {
+            System.out.println("second commit- 02");
     }
 
     public void displayManufacturedDate()
     {
         System.out.println("third commit- 3.0");
     }
-    }
         public void diplayExpiryDat(){
             System.out.println("fouth commit - 04" );
         }
 
     public static void main(String args[]) {
-
         TestPoc testPoc = new TestPoc();
         testPoc.displayVersion();
-        testPoc.displayManufacturedDate();
         testPoc.displayName();
         testPoc.diplayExpiryDat();
 
