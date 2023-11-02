59e9fdab70cf8e92e52a4c97e9bab905751b0841
gituserpr707 <prashantangadi1999@gmail.com>
2023-11-02 13:22:41 +0530

    inital commmit - 01

---
+++ src/TestPoc.java
@@ -0,0 +1,13 @@
+public class TestPoc {
+    public void displayVersion()
+    {
+        System.out.println("initial commit- 1.0");
+    }
+    public static void main(String args[]) {
+
+        TestPoc testPoc = new TestPoc();
+        testPoc.displayVersion();
+    }
+
+
+}
