2a5e807f65622f14bd8a3a89424529310e0364c1
gituserpr707 <prashantangadi1999@gmail.com>
2023-11-02 13:54:53 +0530

    fourth commmit in feature branch02 - 04

--- src/TestPoc.java
+++ src/TestPoc.java
@@ -15,12 +15,19 @@ public class TestPoc {
     {
         System.out.println("third commit- 3.0");
     }
+    }
+        public void diplayExpiryDat(){
+            System.out.println("fouth commit - 04" );
+        }
+
     public static void main(String args[]) {
 
         TestPoc testPoc = new TestPoc();
         testPoc.displayVersion();
         testPoc.displayManufacturedDate();
         testPoc.displayName();
+        testPoc.diplayExpiryDat();
+
     }
 
 
